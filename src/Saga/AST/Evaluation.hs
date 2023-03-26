bea3c08b9adabe64987559760be4cfe99aaf99ba
tiansivive <vilaverde@exabel.com>
2023-03-26 18:15:26 +0200

    builtin binary ops

--- src/Saga/AST/Evaluation.hs
+++ src/Saga/AST/Evaluation.hs
@@ -10,6 +10,11 @@ import qualified Data.Map                   as Map
 
 import           Control.Monad.State.Lazy
 
+
+
+builtInEnv :: [String]
+builtInEnv = ["+", "-","*", "^", "/", "%", "==", "!=", "||", "&&"]
+
 data Value a
   = VInt Int
   | VBool Bool
@@ -18,11 +23,13 @@ data Value a
   | VTuple [Value a]
   | VRecord [(String, Value a)]
   | VClosure [Name a] (Expr a) (Env a)
+  | BuiltIn String
   deriving (Show)
 
 type Env a = Map.Map String (Value a)
 
 type EvalState a = StateT (Env a) (Either String) (Value a)
+
 
 
 eval :: Expr a -> EvalState a
@@ -52,20 +59,39 @@ eval (Block _ defs e) =
 eval (Lambda _ args body) = do
   VClosure args body <$> get
 eval (Identifier (Name _ name)) =
-  let
-    name' = BS.unpack name
-    errorMsg = "Undefined identifier " <> name'
-  in do
-    env <- get
-    let val = Map.lookup name' env
-    lift $ fromMaybe errorMsg val
+    let
+      name' = BS.unpack name
+      errorMsg = "Undefined identifier " <> name'
+    in if name' `elem` builtInEnv
+      then return $ BuiltIn name'
+      else  do
+        env <- get
+        let val = Map.lookup name' env
+        lift $ fromMaybe errorMsg val
 
 eval (FnApp _ fnExpr argExprs) = do
   vals <- mapM eval argExprs
   closure <- eval fnExpr
   env <- get
+  let (VInt x) = vals !! 0
+  let (VInt y) = vals !! 1
+
+  let (VBool b1) = vals !! 0
+  let (VBool b2) = vals !! 1
+
 
   case closure of
+    (BuiltIn "+") -> lift $ Right $ VInt (x + y)
+    (BuiltIn "-") -> lift $ Right $ VInt (x - y)
+    (BuiltIn "*") -> lift $ Right $ VInt (x * y)
+    (BuiltIn "/") -> lift $ Right $ VInt (x `div` y)
+    (BuiltIn "%") -> lift $ Right $ VInt (x `mod` y)
+
+    (BuiltIn "||") -> lift $ Right $ VBool (b1 || b2)
+    (BuiltIn "&&") -> lift $ Right $ VBool (b1 && b2)
+    (BuiltIn "==") -> lift $ Right $ VBool (b1 == b2)
+    (BuiltIn "!=") -> lift $ Right $ VBool (b1 /= b2)
+
     (VClosure paramNames bodyExpr capturedEnv) ->
       if length paramNames == length vals then
         let
